30fed89b5e5546ecffc5ac9fa04e151c58d0b3df
Pavan Gowda <pavangowda@Pavans-MacBook-Air.local>
2020-11-28 18:35:15 +0530

    for test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className='App'>hello there</div>;
+  return <div className='App'>hello there coming soon </div>;
 }
 
 export default App;
